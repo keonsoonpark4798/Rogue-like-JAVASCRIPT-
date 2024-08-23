19baaf461e3ec3dbe0f936c1989c95aa353f231b
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-08-23 18:10:26 +0900

    util.js추가 + 코드 정리

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import readlineSync from 'readline-sync';
+import * as util from './lib/util.js';
 
 class Player {
   constructor(name,hp,atk,x,luk,combo,def) {
@@ -7,7 +8,8 @@ class Player {
     this._hp = hp;                // 최대 체력
     this._currentHP = hp;         // 현재 체력
     this._atk = atk;              // 최소 공격력
-    this._maxatk = x;             // 최대 공격력 배율
+    this._x = x;                  // 최대 공격력 배율
+    this._maxatk = atk + atk * x; // 최대 공격력
     this._luk = luk;              // 도망 확률
     this._combo = combo;          // 연속 공격 확률
     this._def = def;              // 방어확률
@@ -41,7 +43,14 @@ class Player {
     this._atk = value;
   }
 
-  get maxatk(){  // 최대 공격력 배율 get,set
+  get x(){  // 최대 공격력 배율 get,set
+    return this._x;
+  }
+  set x(value){
+    this._x = value;
+  }
+
+  get maxatk(){  // 최대 공격력 get,set
     return this._maxatk;
   }
   set maxatk(value){
@@ -70,13 +79,14 @@ class Player {
   }
 
   attack(monster) {  // 공격 함수
-    let damage =  Math.floor(Math.random() * ((this._atk + Math.ceil(this._atk * this._maxatk)) - this._atk + 1) + this._atk);
+    let damage =  util.random(this.atk,this.maxatk-this.atk+1);
     monster.damage(damage);
     return chalk.green(`몬스터에게 ${damage}의 데미지를 주었습니다.`);
   }
   Parrying(monster) {  // 방어 성공 함수
-    monster.damage(this._atk * 0.6);
-    return chalk.green(`몬스터에게 ${this._atk * 0.6}의 데미지가 반사되었습니다.`);
+    let parryingdamage = Math.round(this._atk * 0.6);
+    monster.damage(parryingdamage);
+    return chalk.green(`몬스터에게 ${parryingdamage}의 데미지가 반사되었습니다.`);
   }
   damage(damage){  // 데미지 함수
     this._currentHP -= damage;
@@ -85,36 +95,47 @@ class Player {
     this._currentHP = this._hp;
   }
   levelup(){  // 스테이지 클리어시 레벨업 함수
-    const ran = Math.floor(Math.random() * 5) + 1;
-    let num = 0;
-    switch(ran)
+    console.log(chalk.green(`레벨업!!`));
+    for(let i = 0; i<3; i++)
     {
-      case 1: // 체력 업
-        num = Math.floor(Math.random() * 30) + 20;
-        this._hp += num;
-        break;
-      case 2: // 최소 공격력 업
-        num = Math.floor(Math.random() * 15) + 5;
-        this._atk += num;
-        break;
-      case 3: // 최대 공격력 배율 업
-        num = Math.floor(Math.random() * 0.9) + 0.1;
-        this._maxatk += num;
-        break;
-      case 4: // 도망 확률 업
-        num = Math.floor(Math.random() * 2) + 1;
-        this._luk += num;
-        break;
-      case 5: // 연속 공격 확률 업
-        num = Math.floor(Math.random() * 4) + 3;
-        this._combo += num;
-        break;
-      case 6: // 방어 수치 업(방어 확률 업)
-        num = Math.floor(Math.random() * 7) + 3;
-        this._def += num;
-        break;
-      default:
-        break;
+      const ran = util.random(1,5);
+      let num = 0;
+      switch(ran)
+      {
+        case 1: // 체력 업
+          num = util.random(20,30); // 20 ~ 50
+          this.hp += num;
+          console.log(chalk.green(`체력이 ${num}만큼 상승했습니다.`));
+          break;
+        case 2: // 최소 공격력 업
+          num = util.random(5,15);  // 5 ~ 20
+          this.atk += num;
+          console.log(chalk.green(`공격력이 ${num}만큼 상승했습니다.`));
+          break;
+        case 3: // 최대 공격력 배율 업
+          num = util.random(1,9)/10; // 0.1 ~ 0.9
+          this.x += num;
+          this.maxatk = Math.ceil(this.atk + this.atk * this.x);
+          console.log(chalk.green(`최대공격력배율이 ${num}만큼 상승했습니다.`));
+          break;
+        case 4: // 도망 확률 업
+          num = util.random(1,2); // 1 ~ 3
+          this.luk += num;
+          console.log(chalk.green(`도망확률이 ${num}만큼 상승했습니다.`));
+          break;
+        case 5: // 연속 공격 확률 업
+          num = util.random(3,4); // 3 ~ 7
+          this.combo += num;
+          console.log(chalk.green(`연속 공격 확률이 ${num}만큼 상승했습니다.`));
+          break;
+        case 6: // 방어 수치 업(방어 확률 업)
+          num = util.random(3,7);  // 3 ~ 10
+          this.def += num;
+          console.log(chalk.green(`방어 확률이 ${num}만큼 상승했습니다.`));
+          break;
+        default:
+          break;
+      }
     }
   }
 }
@@ -122,7 +143,7 @@ class Player {
 class Monster extends Player{
   constructor(stage,hp,atk) {
     super(hp);
-    let ran =  (Math.floor(Math.random() * 6) + 5) / 10;
+    let ran =  util.random(1,8) / 10; // 0.1 ~ 0.8
     this._hp = hp * stage * ran;
     this._atk = Math.ceil(atk * stage * ran);
   }
@@ -141,7 +162,7 @@ function displayStatus(stage, player, monster) {
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
     chalk.blueBright(
-      `| ${player.name} HP: ${player.currentHp}, Attack: ${player.atk}-${player.atk + Math.ceil(player.atk * player.maxatk)}`,
+      `| ${player.name} HP: ${player.currentHp}, Attack: ${player.atk}-${player.maxatk}`,
     ) +
     chalk.redBright(
       `| 몬스터 정보 HP: ${monster.hp}, Attack: ${monster.atk} |`,
@@ -168,7 +189,7 @@ const battle = async (stage, player, monster) => {
 
     // 플레이어의 선택에 따라 다음 행동 처리
     logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
-    let ran = Math.random() * 99 + 1;
+    let ran = util.random(1,99);
     switch(choice)
     {
         case "1" : // 공격
@@ -208,12 +229,13 @@ const battle = async (stage, player, monster) => {
             break;
         case "5" : // 아이템 획득
             if(ran<=10) {
-              let randomItem = Math.floor((Math.random() * 3) + 1);
+              let randomItem = util.random(1,3);
               switch(randomItem)
               {
                 case 1:
                   logs.push(chalk.green(`검을 발견했습니다!(공격력 1 증가)`));
                   player.atk += 1;
+                  player.maxatk = player.atk + player.atk * player.x;
                   break;
                 case 2:
                   logs.push(chalk.green(`런닝화를 발견했습니다!(도망확률 2 증가)`));
@@ -239,10 +261,10 @@ const battle = async (stage, player, monster) => {
             logs.push(monster.attack(player));
             break;
         default :
-        logs.push(chalk.green(`잘못된 선택`));
+        logs.push(chalk.red(`잘못된 선택`));
             break;
     }
-
+    logs.push(chalk.green(`${player.currentHp}만큼 체력이 남았습니다.`));
     if(monster.hp <= 0 || run === true)
     {
       logs.push(chalk.green(`스테이지${stage} 클리어!`));
@@ -250,7 +272,17 @@ const battle = async (stage, player, monster) => {
     }
   }
 };
-
+const rise = async () => {
+  const choice = readlineSync.question('휴식을 마치고 탑을 오르겠습니까?(yes/no)');
+  switch(choice)
+  {
+    case 'yes':
+      break;
+    default:
+      rise();
+      break;
+  }
+};
 export async function startGame(cheat, playername) {
   console.clear();
   if(playername == null)
@@ -275,7 +307,7 @@ export async function startGame(cheat, playername) {
   let stage = 1;
 
   while (stage <= 10) {
-    const monster = new Monster(stage,100,3);
+    const monster = new Monster(stage,100,5);
     await battle(stage, player, monster);
     
     if(player.currentHp <= 0){
@@ -286,6 +318,7 @@ export async function startGame(cheat, playername) {
     else {
       player.levelup();
       player.heal();
+      await rise();
       stage++;
     }
   }
---
+++ test.js
@@ -0,0 +1,3 @@
+import * as util from './lib/util.js';
+
+console.log(util.random(5,6)/10);
